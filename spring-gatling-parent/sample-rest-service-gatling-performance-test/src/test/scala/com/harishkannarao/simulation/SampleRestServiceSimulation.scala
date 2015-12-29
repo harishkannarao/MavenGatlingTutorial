f30c15aff1d93b81078371edc780488862bd0c67
Harish Kannarao <harish.gtec@gmail.com>
2015-12-29 17:22:11 +0000

    Tech: Changed to work for spring boot rest service

--- spring-gatling-parent/sample-rest-service-gatling-performance-test/src/test/scala/com/harishkannarao/simulation/SampleRestServiceSimulation.scala
+++ spring-gatling-parent/sample-rest-service-gatling-performance-test/src/test/scala/com/harishkannarao/simulation/SampleRestServiceSimulation.scala
@@ -1,63 +1,49 @@
 package com.harishkannarao.simulation
 
-import com.harishkannarao.config.{PropertiesUtil, PerformanceTestConfiguration}
+import com.harishkannarao.config.PropertiesUtil
+import com.harishkannarao.util.SpringUtil
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
-import org.springframework.context.ApplicationContext
-import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import scala.concurrent.duration._
 
 class SampleRestServiceSimulation extends Simulation {
-
-  val ctx: ApplicationContext = new AnnotationConfigApplicationContext(classOf[PerformanceTestConfiguration])
-  val propertiesUtil: PropertiesUtil = ctx.getBean(classOf[PropertiesUtil])
+  val propertiesUtil: PropertiesUtil = SpringUtil.ctx.getBean(classOf[PropertiesUtil])
 
   val httpConf = http
     .baseURL(propertiesUtil.getApplicationUrl) // Here is the root for all relative URLs
-    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // Here are the common headers
-    .doNotTrackHeader("1")
-    .acceptLanguageHeader("en-US,en;q=0.5")
-    .acceptEncodingHeader("gzip, deflate")
-    .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
+    .contentTypeHeader("application/json")
 
-  val headers_10 = Map("Content-Type" -> """application/x-www-form-urlencoded""") // Note the headers specific to a given request
-
-  val scn = scenario("Scenario Name") // A scenario is a chain of requests and pauses
-    .exec(http("request_1")
-      .get("/"))
-    .pause(7) // Note that Gatling has recorder real time pauses
-    .exec(http("request_2")
-      .get("/computers?f=macbook"))
-    .pause(2)
-    .exec(http("request_3")
-      .get("/computers/6"))
-    .pause(3)
-    .exec(http("request_4")
-      .get("/"))
-    .pause(2)
-    .exec(http("request_5")
-      .get("/computers?p=1"))
-    .pause(670 milliseconds)
-    .exec(http("request_6")
-      .get("/computers?p=2"))
-    .pause(629 milliseconds)
-    .exec(http("request_7")
-      .get("/computers?p=3"))
-    .pause(734 milliseconds)
-    .exec(http("request_8")
-      .get("/computers?p=4"))
-    .pause(5)
-    .exec(http("request_9")
-      .get("/computers/new"))
+  val scn = scenario("Basic CRUD operations") // A scenario is a chain of requests and pauses
+    .exec(http("Create Customer")
+      .put("/customer")
+      .body(StringBody(
+        """
+          {"firstName":"Harish","lastName":"Kannarao"}
+        """.stripMargin))
+      .check(status.is(201))
+      .check(jsonPath("$.id").saveAs("id"))
+    )
+    .pause(1) // Note that Gatling has recorder real time pauses
+    .exec(http("Get Customer")
+      .get("/customer/${id}")
+      .check(status.is(200))
+    )
     .pause(1)
-    .exec(http("request_10") // Here's an example of a POST request
-      .post("/computers")
-      .headers(headers_10)
-      .formParam("""name""", """Beautiful Computer""") // Note the triple double quotes: used in Scala for protecting a whole chain of characters (no need for backslash)
-      .formParam("""introduced""", """2012-05-30""")
-      .formParam("""discontinued""", """""")
-      .formParam("""company""", """37"""))
+    .exec(http("Update Customer")
+      .post("/customer")
+      .body(StringBody(
+      """
+        {"firstName":"Harish U","lastName":"Kannarao U","id":"${id}"}
+      """.stripMargin
+      ))
+      .check(status.is(200))
+    )
+    .pause(1)
+    .exec(http("Delete Customer")
+      .delete("/customer/${id}")
+      .check(status.is(200))
+    )
 
   setUp(scn.inject(atOnceUsers(1)).protocols(httpConf))
 }
