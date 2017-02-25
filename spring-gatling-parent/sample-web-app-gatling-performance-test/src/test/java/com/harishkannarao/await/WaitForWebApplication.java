2650b1253ed5f44d64f4ee879c94f2b0ad8864fc
harishkannarao <harish.gtec@gmail.com>
2017-02-25 18:11:47 +0000

    Removing antrun maven plugin

---
+++ spring-gatling-parent/sample-web-app-gatling-performance-test/src/test/java/com/harishkannarao/await/WaitForWebApplication.java
@@ -0,0 +1,21 @@
+package com.harishkannarao.await;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.given;
+
+public class WaitForWebApplication {
+    private static final String WEB_APPLICATION_PING_URL = "http://localhost:9100/ping.txt";
+
+    public static void main(String[] args) {
+        RestTemplate restTemplate = new RestTemplate();
+        given().ignoreExceptions().await().atMost(120, SECONDS).until(() -> {
+            ResponseEntity<String> response = restTemplate.exchange(WEB_APPLICATION_PING_URL, HttpMethod.GET, null, String.class);
+            assertThat(response.getStatusCode().is2xxSuccessful()).isTrue();
+        });
+    }
+}
